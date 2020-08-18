9eddb8136740da5fc3ef2f4715df7a7d4170a8c8
Victor Franco <vfrancohdez1@hotmail.com>
2020-08-17 21:37:52 -0500

    added custom tag functionality

--- Video.js
+++ Video.js
@@ -7,8 +7,24 @@ class video_component extends HTMLElement{
         this.shadowRoot.appendChild(this.content);
     }
     connectedCallback(){
+        this.shadowRoot.querySelectorAll("video")[0].setAttribute("src",this.getAttribute("data-src"));
         this.video=new Video(this.shadowRoot.querySelectorAll(".video")[0]);
         this.video.execute();
+        this.shadowRoot.querySelectorAll("video")[0].addEventListener("error",()=>{
+            for(let [key,values] of Object.entries(this.video))
+                if(typeof(values)=="function") this.video[key]=()=>{};
+            this.video=null;
+        });
     }
+    attributeChangedCallback(attr, oldValue, newValue) {
+        if(attr=="data-src"){
+            this.shadowRoot.querySelectorAll("video")[0].setAttribute("src",this.getAttribute("data-src"));
+            if(this.video===null){
+                this.video=new Video(this.shadowRoot.querySelectorAll(".video")[0]);
+                this.video.execute();
+            }
+        }
+    }
+    static get observedAttributes() {return ["data-src"];}
 }
 customElements.define("video-player",video_component);
