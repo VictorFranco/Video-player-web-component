bbe1e574c81927ecff6a087a1be0ea5feafed647
Victor Franco <vfrancohdez1@hotmail.com>
2020-08-19 17:37:34 -0500

    add method to update tag and update reset_tag ()
    Implement a method to update the tag after changing its attribute or after adding it to the page
    
    As the attributeChangedCallback () and connectedCallback () method had the same code, it was decided to create a new method
    
    update_tag () was created
    
    reset_tag () updated due to error
    
    reset_tag () was used by an eventListener (type error), but the listener was not removed and restored, it kept listening and when an error occurred more than once it printed the same error twice
    
    video_error () is called by "onevent handler" instead of "eventListener" because it is easier to remove the function with it

--- Video.js
+++ Video.js
@@ -8,12 +8,16 @@ class video_component extends HTMLElement{
         this.video=null;
     }
     connectedCallback(){
+        this.update_tag();
+    }
+    update_tag(){
         let video_container=this.shadowRoot.querySelectorAll(".video")[0];
         let video_tag=this.shadowRoot.querySelectorAll("video")[0];
+        if(this.getAttribute("data-src")==null) this.setAttribute("data-src","");
         video_tag.setAttribute("src",this.getAttribute("data-src"));
         this.video=new Video(video_container);
         this.video.execute();                    
-        video_tag.addEventListener("error",()=>this.video_error(video_container,video_tag));
+        video_tag.onerror=()=>this.video_error(video_container,video_tag);
     }
     video_error(video_container,video_tag){
         console.log(`%cVideo error`,"font-size:20px",`
@@ -23,16 +27,13 @@ src: ${this.getAttribute("data-src")}`);
         console.log(this);
     }
     reset_tag(tag){
+        let video_tag=this.shadowRoot.querySelectorAll("video")[0];
         let baseNode=tag.cloneNode(true);
+        video_tag.onerror=null;
         tag.parentNode.replaceChild(baseNode,tag);
     }
     attributeChangedCallback(attr, oldValue, newValue) {
-        if(attr=="data-src")
-            this.shadowRoot.querySelectorAll("video")[0].setAttribute("src",this.getAttribute("data-src"));
-        if(oldValue!=newValue&&this.video!=null){
-            this.video=new Video(this.shadowRoot.querySelectorAll(".video")[0]);
-            this.video.execute();
-        }
+        if(attr=="data-src"&&oldValue!=newValue&&this.video!=null) this.update_tag(); 
     }
     static get observedAttributes() {return ["data-src"];}
 }
